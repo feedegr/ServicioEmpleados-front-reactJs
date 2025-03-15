3d7b94810e4e76d2c5e36a2a589d9bedc98a659a
feede.gr <feedecs@hotmail.com>
2025-03-15 13:23:15 -0300

    principio proyecto

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Sistema recursos humanos
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span classNameName="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/agregar">
+                  Agregar empleado
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
